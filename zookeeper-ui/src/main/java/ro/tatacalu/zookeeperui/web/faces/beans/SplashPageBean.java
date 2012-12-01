ca0936a6b05e77074c05923da2b53de2b3ee9535
Matei Dragu <matei.dragu@gmail.com>
2012-12-01 14:47:26 +0100

    wip

--- zookeeper-ui/src/main/java/ro/tatacalu/zookeeperui/web/faces/beans/SplashPageBean.java
+++ zookeeper-ui/src/main/java/ro/tatacalu/zookeeperui/web/faces/beans/SplashPageBean.java
@@ -4,10 +4,21 @@
 package ro.tatacalu.zookeeperui.web.faces.beans;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.netflix.curator.CuratorZookeeperClient;
+import com.netflix.curator.framework.CuratorFramework;
+import com.netflix.curator.framework.CuratorFrameworkFactory;
+import com.netflix.curator.framework.CuratorFrameworkFactory.Builder;
 
 /**
  * @author Tata Calu
@@ -16,8 +27,14 @@ import org.springframework.web.context.WebApplicationContext;
 @Component
 @Scope(WebApplicationContext.SCOPE_SESSION)
 public class SplashPageBean implements Serializable {
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SplashPageBean.class);	
+	
 	private static final long serialVersionUID = -9205351459549012011L;
+	
+	private static final String CONNECT_STRING_LOCALHOST = "127.0.0.1:2181";
+	
+	private List<String> children;
 	
 	/**
 	 * Default constructor.
@@ -25,4 +42,29 @@ public class SplashPageBean implements Serializable {
 	public SplashPageBean() {
 		super();
 	}
+	
+	@PostConstruct
+	public void init() {
+		Builder builder = CuratorFrameworkFactory.builder();
+		CuratorFramework curatorFramework = builder.connectString(CONNECT_STRING_LOCALHOST).build();
+		
+		CuratorZookeeperClient curatorZookeeperClient = curatorFramework.getZookeeperClient();
+		
+		try {
+			this.children = curatorFramework.getChildren().forPath("/");
+			LOGGER.debug("Children: {}", this.children);
+		} catch (Exception e1) {
+			LOGGER.error("Error trying to get the children list", e1);
+		}
+		
+	}
+
+	public List<String> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<String> children) {
+		this.children = children;
+	}
+	
 }
