36db6f63dee1798c32228da373d79e21aaa03cc3
Matei Dragu <matei.dragu@gmail.com>
2013-05-29 01:24:32 +0200

    - Add JUnit 4.11 and spring-test - add logback-test.xml - added test class skeleton

---
+++ zookeeper-ui/src/test/java/ro/tatacalu/zookeeperui/curator/CuratorFrameworkSimpleTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package ro.tatacalu.zookeeperui.curator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Matei
+ *
+ */
+public class CuratorFrameworkSimpleTest {
+	
+	Logger LOGGER = LoggerFactory.getLogger(CuratorFrameworkSimpleTest.class);
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		LOGGER.debug("Test Logging !");
+	}
+
+}
